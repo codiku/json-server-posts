a184c3543f3ff03a517d03e6182c188326d08c1b
Codiku <111140836+codiku-dev@users.noreply.github.com>
2024-01-16 21:04:18 +0100

    wip nexxtjs

---
+++ app/api/posts/constant.ts
@@ -0,0 +1,9 @@
+export const DB = {
+  posts: [
+    {
+      id: 1,
+      title: "This database is reset every 10 records",
+      body: "Better safe than sorry",
+    },
+  ],
+};
---
+++ app/api/posts/route.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { DB } from "./constant";
+import { NextRequest, NextResponse } from "next/server";
+let posts: any[] = [...DB.posts];
+export const GET = async (req: NextRequest) => {
+  try {
+    return NextResponse.json({ posts });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Unable to fetch posts" },
+      { status: 500 }
+    );
+  }
+};
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const newPost = await req.json(); // res now contains body
+    if (posts.length >= 10) {
+      posts = [...DB.posts]; // Reset the database if there are 10 or more posts
+      return NextResponse.json(posts[0]);
+    } else {
+      const postWithId = { ...newPost, id: posts.length + 1 };
+      posts.push(postWithId);
+      return NextResponse.json(postWithId);
+    }
+  } catch (error) {
+    return NextResponse.json({ error: "Unable to add post" }, { status: 500 });
+  }
+};
