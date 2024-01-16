1fa27a100543a1b9e3b706679382ba480775da0f
Codiku <111140836+codiku-dev@users.noreply.github.com>
2024-01-16 21:22:56 +0100

    go

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,6 +1,6 @@
 import { DB } from "./constant";
 import { NextRequest, NextResponse } from "next/server";
-let posts: any[] = [...DB];
+var posts: any[] = [...DB];
 export const GET = async (req: NextRequest) => {
   try {
     return NextResponse.json(posts);
@@ -16,16 +16,27 @@ export const POST = async (req: NextRequest) => {
   try {
     const newPost = await req.json(); // res now contains body
     if (posts.length >= 10) {
-      posts = [...DB]; // Reset the database if there are 10 or more posts
-      return NextResponse.json(posts[0]);
+      posts = [
+        {
+          id: 1,
+          title: "This database is reset every 10 records",
+          author: "Better safe than sorry",
+        },
+      ]; // Reset the database if there are 10 or more posts
+      return NextResponse.json({
+        id: 1,
+        title: "This database is reset every 10 records",
+        author: "Better safe than sorry",
+      });
     } else {
+      console.log(newPost);
       if (newPost.title && newPost.author) {
         const postWithId = { ...newPost, id: posts.length + 1 };
         posts.push(postWithId);
         return NextResponse.json(postWithId);
       } else {
         return NextResponse.json(
-          { error: "Unable to add post invalid body" },
+          { error: "Unable to add post invalid" },
           { status: 500 }
         );
       }
