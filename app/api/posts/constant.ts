5c33209ad64ce00cf05a6d6f405f4bf4a3338d40
Codiku <111140836+codiku-dev@users.noreply.github.com>
2024-01-16 21:09:22 +0100

    posts simple

--- app/api/posts/constant.ts
+++ app/api/posts/constant.ts
@@ -1,9 +1,7 @@
-export const DB = {
-  posts: [
-    {
-      id: 1,
-      title: "This database is reset every 10 records",
-      body: "Better safe than sorry",
-    },
-  ],
-};
+export const DB = [
+  {
+    id: 1,
+    title: "This database is reset every 10 records",
+    body: "Better safe than sorry",
+  },
+];
--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,10 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { DB } from "./constant";
 import { NextRequest, NextResponse } from "next/server";
-let posts: any[] = [...DB.posts];
+let posts: any[] = [...DB];
 export const GET = async (req: NextRequest) => {
   try {
-    return NextResponse.json({ posts });
+    return NextResponse.json(posts);
   } catch (error) {
     return NextResponse.json(
       { error: "Unable to fetch posts" },
@@ -17,7 +17,7 @@ export const POST = async (req: NextRequest) => {
   try {
     const newPost = await req.json(); // res now contains body
     if (posts.length >= 10) {
-      posts = [...DB.posts]; // Reset the database if there are 10 or more posts
+      posts = [...DB]; // Reset the database if there are 10 or more posts
       return NextResponse.json(posts[0]);
     } else {
       const postWithId = { ...newPost, id: posts.length + 1 };
