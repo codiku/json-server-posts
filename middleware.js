eed545732201e2ba677e74c172e447b5987ee3b9
Codiku <111140836+codiku-dev@users.noreply.github.com>
2024-01-16 20:04:33 +0100

    wip

--- middleware.js
+++ middleware.js
@@ -1,19 +1,33 @@
 const fs = require("fs");
-const { readFile } = require("fs/promises");
-
-// hello.js
-module.exports = async (req, res, next) => {
-  let dbJSON = JSON.parse(await readFile("db.json", "utf8"));
-
-  if (dbJSON.posts.length > 10) {
-    dbJSON.posts = [
-      {
-        id: 1,
-        title: "This database is cleared after 10 records",
-        body: "Better be safe than sorry",
-      },
-    ];
-    fs.writeFileSync("./db.json", JSON.stringify(dbJSON));
-  }
-  next();
+const INITIAL_ELEMENT = {
+  id: 1,
+  title: "This database is reset after 10 records",
+  body: "Better be safe than sorry",
 };
+
+const limitWriteMiddleware = async (req, res, next) => {
+  if (req.method === "POST" && req.path === "/posts") {
+    const serverUrl = process.env.VERCEL_URL || "http://localhost:3000";
+    const response = await fetch(`${serverUrl}/posts`);
+    const posts = await response.json();
+    if (posts.length >= 10) {
+      console.log(" les posts ", posts);
+      await fetch(`${serverUrl}/reset`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          posts: [INITIAL_ELEMENT],
+        }),
+      });
+      return res.status(201).json(INITIAL_ELEMENT);
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+};
+
+module.exports = { limitWriteMiddleware };
